ae633582f96be854034aa680ab3e4ad35fc3173b
Mudit Jain <muditj148@gmail.com>
2021-02-04 22:10:39 +0530

    website update

--- app.js
+++ app.js
@@ -667,7 +667,9 @@ function isLoggedIn(req, res, next){
 
 }
 
-app.listen('3000', () => {
+const port = process.env.PORT || '3000'
+
+app.listen(port, () => {
     console.log("Server started at 3000");
 })
 
